27ef0a9b5e1db8dd50cfa815933f8d3a82435175
cswithen <cody.swithenbank@gmail.com>
2022-02-05 21:49:57 -0800

    feat: youtube search
    incorporated youtube search api and axios to be able to use the searchTerm from our search bar to return a list of videos back to us from the youtube api

--- video-browser/src/App.vue
+++ video-browser/src/App.vue
@@ -5,7 +5,11 @@
 </template>
 
 <script>
+import axios from 'axios'
 import SearchBar from './components/SearchBar.vue'
+
+import YOUTUBE_API from '../secrets'
+console.log(YOUTUBE_API)
 
 export default {
     name: 'App',
@@ -14,7 +18,14 @@ export default {
     },
       methods: {
         onTermChange(searchTerm) {
-          console.log(searchTerm)
+          axios.get('https://www.googleapis.com/youtube/v3/search', {
+            params: {
+              key: YOUTUBE_API,
+              type: 'video',
+              part: 'snippet',
+              q: searchTerm
+            }
+          }).then(response => console.log(response))
       }
       },
   }
