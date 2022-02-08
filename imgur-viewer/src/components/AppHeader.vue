f256236f197bf7e9d18590058769208fdf862504
cswithen <cody.swithenbank@gmail.com>
2022-02-08 10:30:53 -0800

    feat: application header component

---
+++ imgur-viewer/src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="ui secondary pointing menu">
+      <a href="/" class="item">
+        Image Storage
+      </a>
+    <div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
