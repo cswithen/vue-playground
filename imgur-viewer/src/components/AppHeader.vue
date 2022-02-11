723c0f9383210ca5643f42090e4fc5179b990f82
cswithen <cody.swithenbank@gmail.com>
2022-02-11 13:11:52 -0800

    feat: localstorage and logout

--- imgur-viewer/src/components/AppHeader.vue
+++ imgur-viewer/src/components/AppHeader.vue
@@ -1,20 +1,31 @@
 <template>
   <div class="ui secondary pointing menu">
-      <a href="/" class="item">
-        Image Storage
-      </a>
+    <a href="/" class="item"> Image Storage </a>
     <div class="right menu">
-      <a href="#" class="ui item" @click="login">Login</a>
+      <div v-if="isLoggedIn" class="horizontal">
+        <a href="" class="item">Galleries</a>
+        <a href="" class="item">Upload</a>
+        <a @click="logout" class="item">Logout</a>
+      </div>
+      <a v-else href="#" class="ui item" @click="login">Login</a>
     </div>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions, mapGetters } from "vuex";
 
 export default {
-  name: 'AppHeader',
-  // use '...mapActions([])' if you have additional methods you want to define specific to the component
-  methods: mapActions(['login'])
-}
+  name: "AppHeader",
+  // use '...mapActions([])' if you have additional methods you want to define specific to the component goes for mapGetters as well
+  methods: mapActions(["login", "logout"]),
+  computed: mapGetters(["isLoggedIn"]),
+};
 </script>
+
+<style scoped>
+.horizontal {
+  display: flex;
+  flex-direction: row;
+}
+</style>
