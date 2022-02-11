1102f7f1085cea5b1ad667b11236bd0407bbef52
cswithen <cody.swithenbank@gmail.com>
2022-02-11 13:21:42 -0800

    feat: vue router navigation

--- imgur-viewer/src/components/AppHeader.vue
+++ imgur-viewer/src/components/AppHeader.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="ui secondary pointing menu">
-    <a href="/" class="item"> Image Storage </a>
+    <router-link to="/" class="item"> Image Storage </router-link>
     <div class="right menu">
       <div v-if="isLoggedIn" class="horizontal">
-        <a href="" class="item">Galleries</a>
-        <a href="" class="item">Upload</a>
+        <router-link to="/" class="item">Galleries</router-link>
+        <router-link to="/upload" class="item">Upload</router-link>
         <a @click="logout" class="item">Logout</a>
       </div>
-      <a v-else href="#" class="ui item" @click="login">Login</a>
+      <a v-else to="#" class="ui item" @click="login">Login</a>
     </div>
   </div>
 </template>
--- imgur-viewer/src/main.js
+++ imgur-viewer/src/main.js
@@ -5,10 +5,16 @@ import { store } from "./store";
 
 // components for routes
 import AuthHandler from "./components/AuthHandler";
+import ImageList from "./components/ImageList";
+import UploadForm from "./components/UploadForm";
 
-const routes = [{ path: "/oauth2/callback", component: AuthHandler }];
+const routes = [
+  { path: "/oauth2/callback", component: AuthHandler },
+  { path: "/upload", component: UploadForm },
+  { path: "/", component: ImageList },
+];
 
-const router = createRouter({
+export const router = createRouter({
   history: createWebHistory(),
   routes,
 });
