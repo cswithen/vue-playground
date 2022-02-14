0502dff253dfd2db8a36ffd507182a8b04651ce1
cswithen <cody.swithenbank@gmail.com>
2022-02-14 11:09:09 -0800

    feat: images state
    added a state of an empty array for the images, incorporated all the state, getters, mutations and actions associated to getting images

--- imgur-viewer/src/api/imgur.js
+++ imgur-viewer/src/api/imgur.js
@@ -1,4 +1,5 @@
 import qs from "qs";
+import axios from "axios";
 
 import { imgurId } from "../../secrets";
 const ROOT_URL = "https://api.imgur.com";
@@ -9,9 +10,16 @@ export default {
       client_id: imgurId,
       response_type: "token",
     };
-
     window.location = `${ROOT_URL}/oauth2/authorize?${qs.stringify(
       querystring
     )}`;
+  },
+
+  fetchImages(token) {
+    return axios.get(`${ROOT_URL}/3/account/me/images`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
   },
 };
