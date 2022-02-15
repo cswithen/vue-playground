1b95ba439cc9fb46b77bd0538f79f7a9cecb504d
cswithen <cody.swithenbank@gmail.com>
2022-02-15 10:11:09 -0800

    style: created css grid styling for ImageList component

--- imgur-viewer/src/components/ImageList.vue
+++ imgur-viewer/src/components/ImageList.vue
@@ -1,5 +1,7 @@
 <template>
-  <img v-for="image in allImages" :src="image.link" :key="image.link" />
+  <div class="image-container">
+    <img v-for="image in allImages" :src="image.link" :key="image.link" />
+  </div>
 </template>
 
 <script>
@@ -14,3 +16,15 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.image-container {
+  column-count: 3;
+  column-gap: 0;
+}
+
+img {
+  max-width: 100%;
+  padding: 5px;
+}
+</style>
