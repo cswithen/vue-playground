0cc63777cb4ed0a6d7dec4ad4e4b8463831a5732
cswithen <cody.swithenbank@gmail.com>
2022-02-15 09:40:21 -0800

    feat: image upload html and styling

--- imgur-viewer/src/components/UploadForm.vue
+++ imgur-viewer/src/components/UploadForm.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>Upload Form</div>
+  <div class="dropper">
+    <input type="file" />
+    <span>Drag files here!</span>
+  </div>
 </template>
 
 <script>
@@ -7,3 +10,25 @@ export default {
   name: "UploadForm",
 };
 </script>
+
+<style scoped>
+.dropper {
+  position: relative;
+  height: 30vh;
+  border: 2px dashed black;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.dropper:hover {
+  background-color: #eee;
+}
+input {
+  cursor: pointer;
+  width: 100%;
+  height: 30vh;
+  position: absolute;
+  opacity: 0;
+}
+</style>
